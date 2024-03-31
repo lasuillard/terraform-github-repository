f5e1d556fc9243cb3f4b5db876245d1550d8eb65
Yuchan Lee <lasuillard@gmail.com>
2024-03-31 13:40:19 +0000

    fix: Output validation error on disabled example

--- outputs.tf
+++ outputs.tf
@@ -1,46 +1,46 @@
 output "repository_full_name" {
   description = "A string of the form \"orgname/reponame\"."
-  value       = github_repository.this[0].full_name
+  value       = try(github_repository.this[0].full_name, null)
 }
 
 output "repository_html_url" {
   description = "URL to the repository on the web."
-  value       = github_repository.this[0].html_url
+  value       = try(github_repository.this[0].html_url, null)
 }
 
 output "repository_ssh_clone_url" {
   description = "URL that can be provided to git clone to clone the repository via SSH."
-  value       = github_repository.this[0].ssh_clone_url
+  value       = try(github_repository.this[0].ssh_clone_url, null)
 }
 
 output "repository_http_clone_url" {
   description = "URL that can be provided to git clone to clone the repository via HTTPS."
-  value       = github_repository.this[0].http_clone_url
+  value       = try(github_repository.this[0].http_clone_url, null)
 }
 
 output "repository_git_clone_url" {
   description = "URL that can be provided to git clone to clone the repository anonymously via the git protocol."
-  value       = github_repository.this[0].git_clone_url
+  value       = try(github_repository.this[0].git_clone_url, null)
 }
 
 output "repository_svn_url" {
   description = "URL that can be provided to svn checkout to check out the repository via GitHub's Subversion protocol emulation."
-  value       = github_repository.this[0].svn_url
+  value       = try(github_repository.this[0].svn_url, null)
 }
 
 output "repository_node_id" {
   description = "GraphQL global node id for use with v4 AP."
-  value       = github_repository.this[0].node_id
+  value       = try(github_repository.this[0].node_id, null)
 }
 
 output "repository_repo_id" {
   description = "GitHub ID for the repositor."
-  value       = github_repository.this[0].repo_id
+  value       = try(github_repository.this[0].repo_id, null)
 }
 
 output "repository_primary_language" {
   description = "The primary language used in the repository."
-  value       = github_repository.this[0].primary_language
+  value       = try(github_repository.this[0].primary_language, null)
 }
 
 output "repository_pages" {
@@ -51,10 +51,10 @@ The block consisting of the repository's GitHub Pages configuration with the fol
 - `html_url`: The absolute URL (including scheme) of the rendered GitHub Pages site e.g. https://username.github.io.
 - `status`: The GitHub Pages site's build status e.g. building or built.
 EOT
-  value       = github_repository.this[0].pages
+  value       = try(github_repository.this[0].pages, null)
 }
 
 output "repository_branch_default" {
   description = "Default branch name."
-  value       = github_branch_default.this[0].branch
+  value       = try(github_branch_default.this[0].branch, null)
 }
