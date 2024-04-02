a65cde5fb331d47126482948028209a5d4e8f274
Yuchan Lee <lasuillard@gmail.com>
2024-04-02 13:27:12 +0000

    refactor: Change type of input `topics` to `set(string)`

--- variables.tf
+++ variables.tf
@@ -225,7 +225,7 @@ The list of topics of the repository.
 NOTE: This attribute is not compatible with the `github_repository_topics` resource. Use one of them.
 `github_repository_topics` is only meant to be used if the repository itself is not handled via terraform, for example if it's only read as a datasource (see issue [#1845](https://github.com/integrations/terraform-provider-github/issues/1845)).
 EOT
-  type        = list(string)
+  type        = set(string)
   default     = null
 }
 
