39ed164bbe228a42b07c4ce25aaedfce66194934
Yuchan Lee <lasuillard@gmail.com>
2024-09-21 22:27:07 +0900

    chore(devcontainer): Update `onCreateCommand.sh`
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get install -y bash-completion
-
-echo '
-if [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-fi
-
-alias tf=terraform
-' >> ~/.bashrc
+sudo apt-get update && sudo apt-get install -y \
+    bash-completion
